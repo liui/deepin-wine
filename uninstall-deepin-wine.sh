859f14651bf7b47e872afd0d8d74472aa70940a0
刘 <liuI@users.noreply.github.com>
2020-03-22 14:16:56 +0800

    Delete uninstall-deepin-wine.sh

--- uninstall-deepin-wine.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-echo "uninstalling deepin-wine"
-sudo apt purge deepin-fonts-wine deepin-wine-helper deepin-wine-plugin deepin-wine-uninstaller -y
-sudo apt install -fy
-sudo apt autoremove -y
-echo "done."
